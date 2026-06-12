33cb9a4a5302aed52e4f1e028a14be28ee22f438
Filippo Crotti <f.crotti@filcronet.com>
2026-06-12 12:58:57 +0200

    fix: require at least one destination when saving a policy

--- gui/src/components/policies/PolicySheet.vue
+++ gui/src/components/policies/PolicySheet.vue
@@ -230,7 +230,7 @@ const schema = z.object({
   retention_keep_yearly: z.coerce.number().int().min(0),
 
   // Destination IDs in priority order (index 0 = priority 1).
-  ordered_destination_ids: z.array(z.string()),
+  ordered_destination_ids: z.array(z.string()).min(1, 'At least one destination is required'),
 
   hook_pre: hookFieldSchema,
   hook_post: hookFieldSchema,
@@ -374,7 +374,7 @@ const { value: retMonthlyValue, errorMessage: retMonthlyError } = useField<numbe
 const { value: retYearlyValue, errorMessage: retYearlyError } = useField<number>('retention_keep_yearly')
 
 // Destinations
-const { value: orderedDestIds } = useField<string[]>('ordered_destination_ids')
+const { value: orderedDestIds, errorMessage: orderedDestIdsError } = useField<string[]>('ordered_destination_ids')
 
 function toggleDest(id: string) {
   const arr = [...(orderedDestIds.value ?? [])]
@@ -1171,6 +1171,7 @@ function onOpenChange(value: boolean) {
               </div>
             </div>
           </div>
+          <p v-if="orderedDestIdsError" class="text-sm text-destructive">{{ orderedDestIdsError }}</p>
 
           <Separator />
 
